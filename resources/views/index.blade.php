a3f36d19ee81dd93676755b2d20b1b6edcbd891c
Okamoto Kazuki <okamotokazuki@OkamotonoMacBook-Air.local>
2021-10-19 08:14:49 +0900

    index.php作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts.foot")
+
+@section("title","目次")
+
+@section("content")
+  <h1>コウヘイへのメッセージ</h1>
+
+
+
+  
+
+  <a class="btn btn-primary" href="#" role="button">コウヘイに想いを届けたい</a>
+
+  <a class="btn btn-primary" href="#" role="button">コウヘイの部屋</a>
+
+@endsection
