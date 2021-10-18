8c6ec749e20aa7536f9f514dfdde6e26475709cb
Okamoto Kazuki <okamotokazuki@OkamotonoMacBook-Air.local>
2021-10-19 07:54:19 +0900

    footerレイアウト作成

---
+++ resources/views/layouts/foot.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>＠yield("title")</title>
+</head>
+
+<body>
+
+＠yield("content")
+
+</body>
+
+
+<footer>
+    <p>本アプリは、個人情報に配慮し、開示情報を制限しております。
+        <br>詳細な内容を閲覧されたい方は、コウヘイ・あるいは彼のご家族にご依頼をお願いします。
+        <br>また、動画投稿を希望される方は、本アプリ製作者へ直接ご連絡ください。
+    </p>
+</footer>
