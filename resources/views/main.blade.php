5fe63317f22b6b2dd2faa48b374eaa301354b084
Okamoto Kazuki <okamotokazuki@OkamotonoMacBook-Air.local>
2021-10-20 18:49:38 +0900

    全体像完成

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,16 +5,19 @@
 <p>見えますか？</p>
 
 <div style="position: relative; padding-bottom: 10%;">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/-e-dZNRsYtY" title="YouTube video player" frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-  </iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Pj0b9f_3nvM" title="YouTube video player" frameborder="0" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
 <div style="position: relative; padding-bottom: 10%;">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/uX0np5A1kKY" title="YouTube video player" frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OUSEtrXXvbY" title="YouTube video player" frameborder="0" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
+<div style="position: relative; padding-bottom: 10%;">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/aJVRrigDJbw" title="YouTube video player" frameborder="0" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
 
 <h1>コウヘイへのメッセージ</h1>
 
